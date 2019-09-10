123d07948e2407d8884069b991d62f9a3a183428
Keri Deemer <kerid18@att.net>
2019-09-10 17:42:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase != phrase.upcase
     return "HUH? SPEAK UP, SONNY!"
-  elsif
-    return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 
